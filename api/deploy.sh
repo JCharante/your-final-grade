ee24b2fb83a9144c7ac1f0f1de5a096c5859ae64
JCharante <git@jcharante.com>
2021-02-07 08:04:47 -0500

    update name of environmental variable for mongo uri in deploy script

--- api/deploy.sh
+++ api/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-CMD="docker service create --name yfg-api -p 3001:3001 -e MONGO_URI=\"${YFG_API_ATLAS_URL}\" jcharante/yfg-api"
+CMD="docker service create --name yfg-api -p 3001:3001 -e YFG_API_ATLAS_URL=\"${YFG_API_ATLAS_URL}\" jcharante/yfg-api"
 CONT="docker-machine ssh nyc-drone-1 '${CMD}'"
 docker build -t jcharante/yfg-api . && docker push jcharante/yfg-api && eval "docker-machine ssh nyc-drone-1 '${CMD}'"
